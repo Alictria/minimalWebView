82d00872c3b803634b2a24809e8a5a69441ab0d3
Patricia Altorfer <patricia.altorfer@digitecgalaxus.ch>
2024-05-15 14:56:49 +0200

    Add Textfield to change displayed WebView

--- app/src/main/java/com/example/minimalwebviewapp/MainActivity.kt
+++ app/src/main/java/com/example/minimalwebviewapp/MainActivity.kt
@@ -6,31 +6,55 @@ import android.view.ViewGroup
 import android.webkit.WebView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.viewinterop.AndroidView
-import com.example.minimalwebviewapp.ui.theme.MinimalWebViewAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        enableEdgeToEdge()
         setContent {
-            WebViewScreen()
+
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                var value by remember {
+                    mutableStateOf("https://www.example.com/")
+                }
+                var url: String? by remember {
+                    mutableStateOf(null)
+                }
+                TextField(
+                    value = value,
+                    onValueChange = { value = it },
+                    singleLine = true,
+                    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                    keyboardActions = KeyboardActions(
+                        onDone = { url = value }
+                    ),
+                )
+                WebViewScreen(url)
+            }
         }
     }
 }
 
 @SuppressLint("SetJavaScriptEnabled")
 @Composable
-fun WebViewScreen() {
+fun WebViewScreen(webVieUrl: String?) {
     Box(modifier = Modifier.fillMaxSize()) {
         AndroidView(
             factory = {
@@ -44,7 +68,9 @@ fun WebViewScreen() {
             update = { webView ->
                 webView.settings.javaScriptEnabled = true
                 webView.settings.domStorageEnabled = true
-                webView.loadUrl("https://www.example.com")
+                if (webVieUrl != null) {
+                    webView.loadUrl(webVieUrl)
+                }
             },
         )
     }
